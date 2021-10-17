f68b7ecaab00c6c35064e2bc25c02bb2a52f192f
Abu Hasan <hasanabubachelor@gmail.com>
2021-10-17 10:58:53 +0600

    title & artist added

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const searchMusic = () => {
   const url = `https://api.lyrics.ovh/suggest/${searchInput}`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => showData(data));
-    
+    .then((data) => showData(data.data));
 };
 
-const showData = allData => {
-    const lyricsName = allData.data[0].title;
-    console.log(lyricsName);
-   const musicData = `
-    <div class="col-md-9">
-                        <h3 class="lyrics-name">Purple Noon</h3>
-                        <p class="author lead">Album by <span>Washed Out</span></p>
+const showData = (songs) => {
+  const singleResult = document.querySelector(".search-result");
+  //   const lyricsName = songs.data[0].title;
+  //   const artistName = songs.data[0].artist.name;
+  songs.forEach((song) => {
+    const songDiv = document.createElement("div");
+    songDiv.className = `single-result row align-items-center my-3 p-3`;
+    songDiv.innerHTML = `
+                       <div class="col-md-9">
+                        <h3 class="lyrics-name">${song.title}</h3>
+                        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                        <audio controls>
+                            <source src="${song.preview}" type="audio/mp3">
+                        </audio>
                     </div>
+                    <div class="col-md-3 text-md-right text-center">
+                        <button onclick="getLyrics()" class="btn btn-success">Get Lyrics</button>
+                    </div>
+                   
    `;
-}
+    singleResult.appendChild(songDiv);
+  });
+};
+
